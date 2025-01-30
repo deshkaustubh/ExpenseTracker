6ef54c3660293293b3b97424e580417414d89d32
Kaustubh <ksd.tcet@gmail.com>
2025-01-30 12:41:48 +0530

    assigned inter font

--- app/src/main/java/com/example/expensetracker/ui/theme/Type.kt
+++ app/src/main/java/com/example/expensetracker/ui/theme/Type.kt
@@ -2,9 +2,19 @@ package com.example.expensetracker.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.expensetracker.R
+
+val InterFont = FontFamily(
+    Font(R.font.inter_regular),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semi_bold, FontWeight.SemiBold),
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_extra_bold, FontWeight.ExtraBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
