04faa26bc06978d9632240f12a23f3a0a2d04145
Kaustubh <ksd.tcet@gmail.com>
2025-01-31 12:03:57 +0530

    Adding date to AddExpense remains

--- app/src/main/java/com/example/expensetracker/ui/theme/AddExpense.kt
+++ app/src/main/java/com/example/expensetracker/ui/theme/AddExpense.kt
@@ -2,6 +2,7 @@ package com.example.expensetracker.ui.theme
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -14,11 +15,18 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,11 +34,13 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.expensetracker.R
+import com.example.expensetracker.widget.ExpenseTextView
 
 
 @Composable
@@ -94,6 +104,19 @@ fun AddExpense() {
 
 @Composable
 fun DataForm(modifier: Modifier) {
+    val name = remember {
+        mutableStateOf("")
+    }
+    val amount = remember {
+        mutableStateOf("")
+    }
+    val date = remember {
+        mutableStateOf(0L)
+    }
+    val dateDialogVisibility = remember {
+        mutableStateOf(false)
+    }
+
     Column(
         modifier = modifier
             .padding(16.dp)
@@ -105,35 +128,36 @@ fun DataForm(modifier: Modifier) {
             .verticalScroll(rememberScrollState())
            // .height(200.dp)
     ) {
-        Text(text = "Type", fontSize = 14.sp)
-        Spacer(modifier = Modifier.size(4.dp))
-        OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.fillMaxWidth())
 
+        ExpenseTextView(text = "Name", fontSize = 14.sp)
         Spacer(modifier = Modifier.size(8.dp))
-
-        Text(text = "Name", fontSize = 14.sp)
-        Spacer(modifier = Modifier.size(4.dp))
-        OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.fillMaxWidth())
-
+        OutlinedTextField(
+            value = name.value,
+            onValueChange = {
+                name.value = it
+            }, modifier= Modifier.fillMaxWidth())
+       ExpenseTextView(text = "Amount", fontSize = 14.sp)
         Spacer(modifier = Modifier.size(8.dp))
+        OutlinedTextField(
+            value = amount.value,
+            onValueChange = {
+                name.value = it
+            }, modifier = Modifier.fillMaxWidth())
 
-        Text(text = "Category", fontSize = 14.sp)
-        Spacer(modifier = Modifier.size(4.dp))
-        OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.fillMaxWidth())
-
+        //date
+        ExpenseTextView(text = "Date", fontSize = 14.sp)
         Spacer(modifier = Modifier.size(8.dp))
+        OutlinedTextField(
+            value = date.value.toString(),
+             modifier = Modifier
+                .fillMaxWidth()
+                .clickable { dateDialogVisibility.value = true },
+            enabled = false
+        )
 
-        Text(text = "Amount", fontSize = 14.sp)
-        Spacer(modifier = Modifier.size(4.dp))
-        OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.fillMaxWidth())
+        // dropdown
 
-        Spacer(modifier = Modifier.size(8.dp))
-
-        Text(text = "Date", fontSize = 14.sp)
-        Spacer(modifier = Modifier.size(4.dp))
-        OutlinedTextField(value = "", onValueChange = {}, modifier = Modifier.fillMaxWidth())
-
-        Spacer(modifier = Modifier.size(8.dp))
+        //type
 
         Button(
             onClick = { },
@@ -141,15 +165,45 @@ fun DataForm(modifier: Modifier) {
                 .clip(RoundedCornerShape(2.dp))
                 .fillMaxWidth()
         ) {
-            Text(
+            ExpenseTextView(
                 text = "Add Expense",
                 fontSize = 14.sp,
                 color = Color.White
             )
         }
+        if(dateDialogVisibility.value) {
+            ExpenseDatePickerDialog(onDateSelected = {date.value = it
+                                                     dateDialogVisibility = false
+                                                     }, onDismiss = {
+                                                         dateDialogVisibility.value =false
+            })
+        }
     } // -> Column
 } // DatForm Function
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ExpenseDatePickerDialog(
+    onDateSelected: (date: Long) -> Unit, // A call back function
+    onDismiss: () -> Unit  // To handle dismiss of dialog
+) {
+    val datePickerState  =  rememberDatePickerState() // This creates and remembers the state for the date picker
+    val selectedDate = datePickerState.selectedDateMillis  ?: 0L // it retrieves the selected date , if no date is selected 0L which means default or invalid date
+    DatePickerDialog(onDismissRequest = {onDismiss()}, // displays a dialog with date picker
+        confirmButton = { // This button confirms the selected date
+            TextButton(onClick = { onDateSelected(selectedDate) }) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = { // This button cancels the selection
+            TextButton(onClick = { onDateSelected(selectedDate) }) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState) // it is the actual date picker component
+        }
+}
 
 @Composable
 @Preview(showBackground = true)
